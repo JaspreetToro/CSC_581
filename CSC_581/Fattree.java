7795f0ccbe4fa52da4ca20d5ec3e7018bbf5c409
JaspreetToro <43653684+JaspreetToro@users.noreply.github.com>
2019-09-21 14:38:02 -0700

    Update Fattree.java
    Beautify

--- CSC_581/Fattree.java
+++ CSC_581/Fattree.java
@@ -6,22 +6,22 @@ import java.util.Scanner;
 public class Fattree {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hello enter k please.");
 		Scanner in = new Scanner(System.in);
 
 		int k = in.nextInt();
 
 		Map<Integer, String> topoMap = CreateTopology(k);
-		
+
 		System.out.println("print the id for first Pm");
 		int i = in.nextInt();
 		System.out.println("print the id for first Pm");
 		int j = in.nextInt();
-		
+
 		System.out.println(topoMap);
 		NumberOfHops(topoMap, k,i,j);
-		
+
 		in.close();
 	}
 
@@ -29,6 +29,11 @@ public class Fattree {
 	{
 		int hops;
 
+		double temp1 =  Math.floor((2*i)/k);
+		double temp2 = Math.floor((2*j)/k);
+		double temp3 = Math.floor((4*i)/Math.pow(k, 2));
+		double temp4 = Math.floor((4*j)/Math.pow(k, 2));
+		
 		if(i == j)
 		{
 			hops = 0;
@@ -47,18 +52,19 @@ public class Fattree {
 		}
 		else
 		{
+			System.out.println("error");
 			return;
 		}
-		
-		System.out.println(hops);
-		
+
+		System.out.println("Number of Hops: "+ hops);
+
 	}
 
 	@SuppressWarnings("unused")
 	private static Map<Integer, String> CreateTopology(int k) {
 		//initialize map and variables needed
 		Map<Integer, String> map = new HashMap<Integer, String>();
-		
+
 		int total = (int) (((5 * Math.pow(k, 2))+(Math.pow(k, 3)))/4);
 		int numCores = (int) ( Math.pow(k, 2)/4 );
 		int numAgg = (int) (Math.pow(k, 2)/2);
@@ -66,29 +72,29 @@ public class Fattree {
 		int numPm = (int) (Math.pow(k, 3)/4);		
 		int idNum = 0;
 		int localTotal = numPm;
-		
-		
+
+
 		//assign map
 		//assign PMs 
 		while(idNum < localTotal){
 			map.put(idNum, "PM");
 			idNum++;
 		}
-		
+
 		//assign Edge
 		localTotal += numEdge;
 		while(idNum < localTotal) {
 			map.put(idNum, "Edge");
 			idNum++;
 		}
-		
+
 		//assign Aggregation
 		localTotal += numAgg;
 		while(idNum < localTotal) {
 			map.put(idNum, "Aggregation");
 			idNum++;
 		}
-		
+
 		//assign Cores
 		localTotal += numCores;
 		while(idNum < localTotal) {
